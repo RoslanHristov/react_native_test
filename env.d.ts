6fd151177e94c67bfd97f301781877aea2c4ed66
Roslan Hristov <roslanhristov@gmail.com>
2024-06-27 11:52:13 +0300

    inital commit (everything)

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+declare module "@env" {
+  export const ENDPOINT: string;
+  export const PLATFORM: string;
+  export const PROJECT_ID: string;
+  export const DATABASE_ID: string;
+  export const USER_COLLECTION: string;
+  export const VIDEO_COLLECTION: string;
+  export const STORAGE_ID: string;
+}
